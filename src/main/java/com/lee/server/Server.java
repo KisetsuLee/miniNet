5b53cd9a1f0bcfeb555fed6fbeed1282674c1ae6
KisetsuLee <lizhijie_lee@126.com>
2020-09-15 23:08:56 +0800

    添加schema maven-jaxb2-plugin生成对应的Java类 mvn jaxb2:xcj
    服务器延时1s返回200

--- src/main/java/com/lee/server/Server.java
+++ src/main/java/com/lee/server/Server.java
@@ -8,11 +8,13 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Author: Lzj
  * Date: 2020-09-15
  * Description:
+ * 监听8081端口，处理接收到的所有请求，均返回200状态码
  */
 public class Server {
     public static void main(String[] args) {
@@ -33,7 +35,13 @@ public class Server {
     private static class MyHttpHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
-            httpExchange.sendResponseHeaders(200, 0);
+            try {
+                System.out.println("有一个客户端连接了");
+                TimeUnit.SECONDS.sleep(1);
+                httpExchange.sendResponseHeaders(200, 0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
